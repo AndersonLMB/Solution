501dc0e853629a894969206aecca9d22d32fd05a
AndersonLMB <andersonlmb@outlook.com>
2017-04-18 12:37:52 +0800

    TerrainAdded

--- CesiumPipeline/Scripts/Main.js
+++ CesiumPipeline/Scripts/Main.js
@@ -1,1 +1,11 @@
-﻿var viewer = new Cesium.Viewer('cesiumContainer');
+﻿//var viewer = new Cesium.Viewer('cesiumContainer');
+
+var terrainProvider = new Cesium.CesiumTerrainProvider({ url: "http://localhost/CesiumPipeline/canton" });
+var viewer = new Cesium.Viewer("cesiumContainer", {
+    baseLayerPicker: false,
+    terrainProvider: terrainProvider,
+    imageryProvider: new Cesium.ArcGisMapServerImageryProvider({
+        url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer'
+    }),
+});
+
